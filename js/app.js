815ccb935b8590b56aa570314fcf6827436faf15
Joao Andrade <jpedronandardrade@gmail.com>
2025-06-05 23:57:41 -0300

    Title atualiza com quantidade de itens no carrinho

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
-let valorTotal = 0
-limpaCampos()
+let valorTotal = 0;
+let contador = 0;
+limpaCampos();
 
-function limpaCampos(){
-let listaLimpa = document.getElementById('lista-produtos').innerHTML='';
-let carrinhoLimpo = document.getElementById('valor-total').innerHTML='<span class="texto-azul" id="valor-total">R$0</span>';
 
-}
+
 
 function adicionar(){
-let produtoAdicionado = document.getElementById('produto').value
+let produtoAdicionado = document.getElementById('produto').value;
 let nomeProduto = produtoAdicionado.split('-')[0];
 let valorProduto = produtoAdicionado.split('R$')[1];
 let quantidade = document.getElementById('quantidade').value;
@@ -16,25 +14,45 @@ let quantidade = document.getElementById('quantidade').value;
 let valorTotalProduto = quantidade*valorProduto;
 
 if(quantidade== ""){
-    alert('Campo quantidade não pode ficar vazio')
-        return
+    alert('Campo quantidade não pode ficar vazio');
+        return;
 }else{
 let listaCarrinho = document.getElementById('lista-produtos');
 listaCarrinho.innerHTML= listaCarrinho.innerHTML + ` <section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorTotalProduto}</span>
-        </section>`
+        </section>`;
 }
 
 valorTotal += valorTotalProduto;
 
-let totalAparente = document.getElementById('valor-total')
-    totalAparente.innerHTML = `<span class="texto-azul" id="valor-total">R$${valorTotal}</span>`
+let totalAparente = document.getElementById('valor-total');
+    totalAparente.innerHTML = `<span class="texto-azul" id="valor-total">R$${valorTotal}</span>`;
 
-document.getElementById('quantidade').value=''
+document.getElementById('quantidade').value='';
 
-    }
-    
+contador++;
+
+let corrige = contador == 1 ? 'item' : 'itens' ;
+let corrigeDois = contador == 1 ? 'adicionado' : 'adicionados';
+
+alteraTitulo(`(${contador}) ${corrige} ${corrigeDois}`);
+
+}
+
+function limpaCampos(){
+    let listaLimpa = document.getElementById('lista-produtos').innerHTML='';
+    let carrinhoLimpo = document.getElementById('valor-total').innerHTML='<span class="texto-azul" id="valor-total">R$0</span>';
+}
 
     function limpar() {
-   limpaCampos()
-    }
+    limpaCampos();
+    alteraTitulo ('Carrinho de compras');
+}
+
+
+function alteraTitulo (frase){
+    
+    let titulo = document.querySelector('title');
+titulo.innerHTML = frase;
+
+}
